bc34ab18b4edc41926f66b8563c8b46e0b0d09d0
Элина Обухова <45429218+WriteWrote@users.noreply.github.com>
2022-12-11 15:52:29 +0300

    DocumentService: constructor

--- service/src/main/java/test/eo/service/logic/impl/DocumentService.java
+++ service/src/main/java/test/eo/service/logic/impl/DocumentService.java
@@ -1,9 +1,67 @@
 package test.eo.service.logic.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
+import test.eo.db.repositories.DocumentRepository;
+import test.eo.service.logic.IDocumentService;
+import test.eo.service.mapper.impl.DocumentMapper;
+import test.eo.service.model.create.CreateDocumentDto;
+import test.eo.service.model.get.DocumentDto;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @Validated
-public class DocumentService {
+public class DocumentService implements IDocumentService {
+    private final DocumentRepository rep;
+
+    private final DocumentMapper map;
+
+    @Autowired
+    public DocumentService(DocumentRepository rep, DocumentMapper map) {
+        this.rep = rep;
+        this.map = map;
+    }
+
+    @Override
+    public DocumentDto getDocumentById(Integer id) {
+        return Optional.of(re);
+    }
+
+    @Override
+    public DocumentDto getDocumentBySerialNumber(Integer serial) {
+        return null;
+    }
+
+    @Override
+    public CreateDocumentDto createDocument(CreateDocumentDto dto) {
+        return null;
+    }
+
+    @Override
+    public List<DocumentDto> getAllDocuments() {
+        return null;
+    }
+
+    @Override
+    public void deleteDocumentById(Integer id) {
+
+    }
+
+    @Override
+    public void deleteDocumentBySerialNumber(Integer serial) {
+
+    }
+
+    @Override
+    public DocumentDto updateDocument(DocumentDto oldDoc) {
+        return null;
+    }
+
+    @Override
+    public DocumentDto createLink(DocumentDto leftDoc, DocumentDto rightDoc) {
+        return null;
+    }
 }
